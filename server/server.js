c6719ca763bc94cb6a12b2883f1fe09ee4dedfc1
Topash15 <Alextopash@gmail.com>
2021-11-12 16:23:17 -0500

    added ability to start client and server at same time

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth')
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 
@@ -31,6 +32,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
