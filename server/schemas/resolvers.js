6f37c138fd516ee2c54eedd67b7ca7da27c79b34
Topash15 <Alextopash@gmail.com>
2021-11-12 12:19:44 -0500

    added authorization and thoughts,reactions,friends routes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,60 +1,125 @@
-const { User, Thought } = require('../models');
-const { AuthenticationError } = require('apollo-server-express')
+const { User, Thought } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        // gets all thoughts if args are empty
-        // otherwise all thoughts by username
-        thoughts: async (parent, { username }) => {
-            const params = username ? { username } : {}
-            return Thought.find(params).sort({ createdAt: -1 })
-        },
-        // gets thought by id
-        thought: async (parent, { _id }) => {
-            return Thought.findOne({ _id });
-        },
-        // gets all users
-        users: async () => {
-            return User.find()
-                .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts')
-        },
-        // gets single user by id
-        user: async (parent, { _id }) => {
-            return User.findOne({ _id })
-                .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts')
-        }
+  Query: {
+    // gets all thoughts if args are empty
+    // otherwise all thoughts by username
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
     },
-    Mutation: {
-        // creates a new user
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
+    // gets thought by id
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    // gets all users
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    // gets single user by id
+    user: async (parent, { _id }) => {
+      return User.findOne({ _id })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    // gets current user
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("friends")
+          .populate("thoughts");
 
-            return user;
-        },
+        return userData;
+      }
 
-        // logs user in
-        login: async (parent, { email, password})=> {
-            const user = await User.findOne({ email });
+      throw new AuthenticationError('Not logged in')
+    },
+  },
+  Mutation: {
+    // creates a new user
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
-            // if no user is found, return error
-            if (!user) {
-                throw new AuthenticationError('Incorrect credentials')
-            }
+      return { token, user };
+    },
 
-            const correctPw = await user.isCorrectPassword(password);
+    // logs user in
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-            // if password is incorrect or missing, return error
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials')
-            }
+      // if no user is found, return error
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
 
-            return user;
-        },
+      const correctPw = await user.isCorrectPassword(password);
+
+      // if password is incorrect or missing, return error
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // creates a new thought
+    addThought: async (parent, args, context) => {
+        if(context.user) {
+            const thought = await Thought.create({ ...args, username: context.user.username});
+
+            await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $push: { thoughts: thought._id } },
+                { new: true }
+            );
+                console.log (context.user)
+            return thought;
+        }
+
+        throw new AuthenticationError("You need to be logged in to create a new thought!");
+    },
+
+    // creates a new reaction
+    addReaction: async ( parent, { thoughtId, reactionBody}, context ) => {
+        if(context.user){
+            const updatedThought = await Thought.findOneAndUpdate(
+                { _id: thoughtId },
+                { $push: { reactions : {reactionBody, username: context.user.username }}},
+                { new: true, runValidators: true}
+            );
+
+        return updatedThought;
+        }
+
+        throw new AuthenticationError("You need to be logged in to create a new reaction!")
+    },
+
+    // adds a new friend
+    addFriend: async (parent, { friendId }, context ) => {
+
+        if (context.user){
+            console.log({user: context.user , friend: friendId})
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $push: { friends: friendId }},
+                { new: true}
+            ).populate('friends');
+
+            return updatedUser;
+        }
+
+        throw new AuthenticationError("You need to be logged in to add a new friend!");
     }
+  },
 };
 
-module.exports = resolvers
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth')
 
 const { typeDefs, resolvers } = require('./schemas');
 
@@ -9,7 +10,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs, 
     resolvers,
-    // context: authMiddleware
+    context: authMiddleware
   });
 
   // start apollo server
