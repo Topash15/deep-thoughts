d5356beae5245308346c2f152c099022300ddfde
Topash15 <Alextopash@gmail.com>
2021-11-13 10:09:36 -0500

    added login/logout functionality

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -14,44 +14,44 @@ class AuthService {
         // use type coersion to check if token is NOT undefined
         // and token is NOT expired
         return !!token && !this.isTokenExpired(token);
+    }
 
-        // check if token has expired
-        isTokenExpired(token) {
-            try {
-                const decoded = decode(token);
+    // check if token has expired
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
 
-                if (decoded.exp < Date.now() / 1000){
-                    return true
-                } else {
-                    return false
-                }
-            } catch (err) {
+            if (decoded.exp < Date.now() / 1000){
+                return true
+            } else {
                 return false
             }
-        };
-
-        // retrieve token from localstorage
-        getToken(){
-            // retreives user token from local storage
-            return localStorage.getItem('id_token');
-        } 
-
-        // set token to localStorage and relaod page to homepage
-        login(idToken) {
-            // saves user token to local localStorage
-            localStorage.setItem('id_token', idToken);
-
-            window.location.assign('/');
+        } catch (err) {
+            return false
         }
+    };
 
-        // clear token from local storage and force logout with reload
-        logout() {
-            // clear user token and profile data from local storage
-            localStorage.removeItem('id_token');
+    // retrieve token from localstorage
+    getToken(){
+        // retreives user token from local storage
+        return localStorage.getItem('id_token');
+    } 
 
-            // reload page and reset state of application
-            window.location.assign('/');
-        }
+    // set token to localStorage and relaod page to homepage
+    login(idToken) {
+        // saves user token to local localStorage
+        localStorage.setItem('id_token', idToken);
+
+        window.location.assign('/');
+    }
+
+    // clear token from local storage and force logout with reload
+    logout() {
+        // clear user token and profile data from local storage
+        localStorage.removeItem('id_token');
+
+        // reload page and reset state of application
+        window.location.assign('/');
     }
 }
 
